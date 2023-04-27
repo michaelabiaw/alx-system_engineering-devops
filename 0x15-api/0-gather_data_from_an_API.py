3ad2019f3c8cb730db3a6191cdb0c13625f56a27
michaelabiaw <michaelabiaw@gmail.com>
2023-04-27 01:33:11 +0000

    updated

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,18 +1,48 @@
 #!/usr/bin/python3
-"""script using this REST API, for a given employee ID,
-returns information about his/her TODO list progress."""
-import requests as r
+"""Using a public API 'https://jsonplaceholder.typicode.com'"""
+import requests as req
 import sys
 
+# base url of the api
+baseUrl = "https://jsonplaceholder.typicode.com"
+
+
+def get_todos(userId: str):
+    """Fetch All Todos of a User"""
+    res = req.get(
+        "{}/todos?userId={}".format(baseUrl, userId))
+    return res.json()
+
+
+def get_user(userId: str):
+    """Fetch a User"""
+    res = req.get(
+        "{}/users/{}".format(baseUrl, userId)
+    )
+    return res.json()
+
+
+def task_0(userId: str):
+    """Print the result of Task 0"""
+    user = get_user(userId)
+    if user == {}:
+        return
+    todos = get_todos(userId)
+    completed = tuple(filter(
+        lambda x: x["completed"], todos
+    ))
+    name = user.get("name")
+    print(
+        "Employee {} is done with tasks({}/{}):".format(
+            name, len(completed), len(todos)
+        ))
+    for t in completed:
+        print("\t", t.get("title"))
+
+
 if __name__ == '__main__':
-    url = 'https://jsonplaceholder.typicode.com/'
-    usr_id = r.get(url + 'users/{}'.format(sys.argv[1])).json()
-    to_do = r.get(url + 'todos', params={'userId': sys.argv[1]}).json()
-#    print(to_do)
-    completed = [title.get("title") for title in to_do if
-                 title.get('completed') is True]
-    print(completed)
-    print("Employee {} is done with tasks({}/{}):".format(usr_id.get("name"),
-                                                          len(completed),
-                                                          len(to_do)))
-    [print("\t {}".format(title)) for title in completed]
+    try:
+        userId = sys.argv[1]
+        task_0(userId)
+    except IndexError:
+        print("Usage: filenae <user_id>")
